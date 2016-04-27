100af3d14c951f1f0d9f8c355dcef9f51d6cdca8
steveodat <github@quietdisaster.com>
2016-04-27 18:24:23 -0400

    Update twitterfix.user.js
    reworked to prevent multiple style element creations, and to hide the while you were away thing

--- twitterfix.user.js
+++ twitterfix.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name		Hide Twitter Trends and Promoted Tweets (And Also The Moments Tab)
-// @description	See: title.
+// @name		Hide Annoying Twitter Things
+// @description	Hides Trends, Promoted Tweets, Moments Tab, and that While You Were Away Nonsense
 // @include     htt*://twitter.com*
 // @include     htt*://*.twitter.com*
 // @grant		none
 // ==/UserScript==
 
+var head, style;
+head = document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+
 function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+    style.innerHTML = style.innerHTML + css;
 }
 
-addGlobalStyle('.trends {display:none!important;}');
-addGlobalStyle('.promoted-tweet {display:none!important;}');
-addGlobalStyle('.moments {display:none!important;}');
+document.addEventListener("DOMContentLoaded", function() {
+	addGlobalStyle('.trends, .promoted-tweet, .moments, .has-recap {display:none!important;}');
+	head.appendChild(style);
+});
