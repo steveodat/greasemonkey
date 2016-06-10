18a63f6707e373c0671b76814732e111f4073447
steveodat <github@quietdisaster.com>
2016-06-10 18:18:28 -0400

    added a check for trend element existing, de-fixed the element pre-window resize

--- facebookfix.user.js
+++ facebookfix.user.js
@@ -17,20 +17,24 @@ function addGlobalStyle(css) {
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    addGlobalStyle('#pagelet_ego_pane, #pagelet_ego_pane_w, #pagelet_side_ads, #fbDockChatBuddylistNub, #pagelet_friends_online, #listsNav, #interestsNav, #appsNav, #pagesNav, #pagelet_sidebar, #pagelet_rhc_footer, #developerNav, .noshowdy {display: none!important;}');
+    addGlobalStyle('#pagelet_ego_pane, #pagelet_ego_pane_w, #pagelet_side_ads, #fbDockChatBuddylistNub, #pagelet_friends_online, #listsNav, #interestsNav, #appsNav, #pagesNav, #pagelet_sidebar, #pagelet_dock, #pagelet_rhc_footer, #developerNav, .noshowdy {display: none!important;}');
     addGlobalStyle('.showdy {display: block!important;}');
     addGlobalStyle('.tar {text-align: right;}');
     addGlobalStyle('.home_right_column {min-height: 0!important;}');
+    addGlobalStyle('#u_0_q {position: static!important;}');
 
-    document.getElementById("pagelet_trending_tags_and_topics").className = 'pagelet noshowdy';
+    head.appendChild(style);
+    
+    /* make sure this bit exists before doing stuff to it, otherwise it can sometimes bork up the rest of the script */
+    if (document.getElementById("pagelet_trending_tags_and_topics") !== null) {
+       document.getElementById("pagelet_trending_tags_and_topics").className = 'pagelet noshowdy';
 
-    var wra = document.getElementsByClassName("home_right_column");
+       var wra = document.getElementsByClassName("home_right_column");
 
-    var newnode = document.createElement("div");
-    newnode.innerHTML = '<a href="#" onclick="var tt = document.getElementById(\'pagelet_trending_tags_and_topics\'); if (tt.className == \'pagelet showdy\') {tt.className = \'pagelet noshowdy\';} else {tt.className = \'pagelet showdy\';}">show/hide</a>';
-    newnode.className = 'tar';
+       var newnode = document.createElement("div");
+       newnode.innerHTML = '<a href="#" onclick="var tt = document.getElementById(\'pagelet_trending_tags_and_topics\'); if (tt.className == \'pagelet showdy\') {tt.className = \'pagelet noshowdy\';} else {tt.className = \'pagelet showdy\';}">show/hide</a>';
+       newnode.className = 'tar';
 
-    wra[0].appendChild(newnode);
-});
-
-head.appendChild(style);
+       wra[0].appendChild(newnode);
+    }
+ });
