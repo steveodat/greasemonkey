810b3e663bfcab772916594a47d7fb8dddb38e1f
steveodat <github@quietdisaster.com>
2017-11-14 19:44:49 -0500

    youtube fix-os!
    The header on youtube went fixed a long while back.  This undoes that.  And scrolls the page to the top of the video container.  Because when I've got youtube open in a perfectly sized window, it saves the effort or re-aligning it video to video.

---
+++ youtubeheader.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name		Make youtube's fixed header static
+// @description	makes header static not fixed, for annoyance fixing
+// @include     htt*:/*youtube.com*
+// ==/UserScript==
+
+var head, style;
+head = document.getElementsByTagName('head')[0];
+style = document.createElement('style');
+style.type = 'text/css';
+
+function addGlobalStyle(css) {
+    style.innerHTML = style.innerHTML + css;
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+	addGlobalStyle('#masthead-positioner, #masthead-container {position:static!important; transform: none!important;}');
+	addGlobalStyle('#masthead-positioner-height-offset{display: none!important;}');
+	addGlobalStyle('#page-manager{margin-top: 0px!important;}');
+	head.appendChild(style);
+}
+
+// this bit scrolls the page down the top(ish) of the video container in firefox
+// mostly useful when going from video to video
+window.setTimeout(function() {
+  window.scrollTo(0,75);
+},2500);
