8beb3e727c6c5925bd973c29b9c42325361c1e3b
steveodat <github@quietdisaster.com>
2018-09-21 11:14:37 -0400

    removed .mvm class
    removed the .mvm class, as it was screwing with, I think Memories/on this day?  v professional over here.

--- facebookfix.user.js
+++ facebookfix.user.js
@@ -15,8 +15,8 @@ function addGlobalStyle(css) {
 }
 
 if (document.readyState === "complete" || document.readyState === "interactive") {
-    addGlobalStyle('#pagelet_ego_pane, #pagelet_marketplace_new_user_top_picks_rhc, .mvm, #pagelet_ego_pane_w, #pagelet_video_home_suggested_for_you_rhc, #pagelet_ego_contextual_group, #stories_pagelet_rhc, #pagelet_side_ads, #fbDockChatBuddylistNub, #pagelet_friends_online, #listsNav, #interestsNav, #appsNav, #pagesNav, #left_nav_section_nodes, #pinnedNav, #leftCol, #pagelet_sidebar, #pagelet_dock, #pagelet_rhc_footer, #developerNav, .fbChatSidebar, .noshowdy, ._3lb4:not([data-timestamp]) div {display: none!important;}');
-    addGlobalStyle('#u_0_p, .fixed_elem {position: static!important;}');
+    addGlobalStyle('#pagelet_ego_pane, #pagelet_marketplace_new_user_top_picks_rhc, #profile_timeline_tiles_unit_pagelets_fun_fact_answers, ._nvf, #pagelet_ego_pane_w, #pagelet_video_home_suggested_for_you_rhc, #pagelet_ego_contextual_group, #stories_pagelet_rhc, #pagelet_side_ads, #fbDockChatBuddylistNub, #pagelet_friends_online, #listsNav, #interestsNav, #appsNav, #pagesNav, #left_nav_section_nodes, #pinnedNav, #leftCol, #pagelet_sidebar, #pagelet_dock, #pagelet_rhc_footer, #developerNav, .fbChatSidebar, .noshowdy, ._3lb4:not([data-timestamp]) div {display: none!important;}');
+    addGlobalStyle('#u_0_p, .fixed_elem:not(.fbTimelineStickyHeader) {position: static!important;}');
     addGlobalStyle('#contentArea, #rightCol {clear: left!important;float: none!important;position: static!important; margin: 0px auto!important;text-align: left;} ._2yq #globalContainer {width:100%!important;} #contentCol {text-align: center!important; width: 100%!important;margin:0px!important;} #content_container {margin: 0px auto!important;}');
     head.appendChild(style);
 }
