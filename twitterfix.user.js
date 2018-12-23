dd87ce1764999a55a45b759a6f8adf9d21d380ed
steveodat <github@quietdisaster.com>
2018-12-23 17:23:43 -0500

    got rid of the show/hide button
    got rid of the show/hide button, since twitter realized that was dumb and offered an option to do that on their end

--- twitterfix.user.js
+++ twitterfix.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name		Hide Annoying Twitter Things
-// @description	Hides Trends, Promoted Tweets, Moments Tab, that While You Were Away Nonsense. Now also restores non-Segoe UI font.
-// @include     htt*:/*twitter.com*
+// @description	Hides Trends, Promoted Tweets, Moments Tab, that While You Were Away Nonsense. Now also restores non-hideous Segoe UI font.
+// @include     htt*://twitter.co*
+// @include     htt*://*twitter.co*
 // @grant		none
 // ==/UserScript==
 
@@ -20,37 +21,13 @@ function addStyle(css) {
 
 if (maindoc.readyState === "complete" || maindoc.readyState === "interactive") {
   
-	// hide things we do not want to see
-	addStyle('.trends, .promoted-tweet, .moments, .has-recap, .LiveVideoHomePageModuleContainer, .newstream li[data-suggestion-json*="ActivityTweet"] {display:none!important;}');
+	// hide things we do not want to see (promoted twwts, trending topics, the moments tab, etc)
+	addStyle('.trends, .promoted-tweet, .Footer, .moments, .has-recap, .LiveVideoHomePageModuleContainer, .newstream li[data-suggestion-json*="ActivityTweet"] {display:none!important;}');
 
-	// change the font back to Helvetica, because I am v picky and resistant to change YES I AM A STEREOTYPE OF A NERD I GET IT
+	// change the font back to Helvetica, because dang the new one is ugly
 	addStyle('body.ms-windows {font-family:"Helvetica Neue",Helvetica,Arial,sans-serif!important;}');
-
-	// styling for the add/show favs link.  still deciding on location.
-	addStyle('#toglink {position: fixed; left: 0; bottom: 20px; background-color: white; border: 1px solid #AAA; font-size: 13px;line-height: 1em; padding: .4em;cursor: pointer; user-select: none; -moz-user-select: none; -webkit-user-select: none;-ms-user-select: none;}}');
 
 	// add the style element to the head
 	maindoc.getElementsByTagName('head')[0].appendChild(style);
 
-	// create the show/hide link node, set the id and content, and append it to the body
-	var newnode = maindoc.createElement("div");
-	newnode.setAttribute('id','toglink');
-	newnode.innerHTML = '> hide favs';
-	maindoc.body.appendChild(newnode);
-	// get the newly create node for use later on
-	var toglink = maindoc.getElementById('toglink');
-
-	// get classlist fot t timeline div
-	var tlcl = maindoc.getElementById("timeline").classList;
-
-	// add or remove the class newstream to the timeline div to hide or show favs, and change the trigger innerHTML accordingly
-	toglink.addEventListener("click", function() {
-	if (tlcl.contains('newstream')) {
-			tlcl.remove('newstream');
-			toglink.innerHTML = '> hide favs';
-		} else {
-			tlcl.add('newstream');
-			toglink.innerHTML = '> show favs';
-		}
-	});
-});
+}
